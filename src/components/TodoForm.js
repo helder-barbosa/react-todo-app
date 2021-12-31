762a650d75df06cfc9a5ef70bb14cd44f08f7b2f
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-12-30 22:34:19 -0300

    #2 Editing TodoList component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -11,12 +11,10 @@ function TodoForm(props) {
   const handleSubmit = e => {
     e.preventDefault()
 
-    /*
     props.onSubmit({
       id: Math.floor(Math.random() * 1000),
       text: input
     })
-    */
 
     setInput('')
   }
